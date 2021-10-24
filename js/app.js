853ce030f512a7319c52aa16b7cb83636b80d73f
Leandro Montivero <leandromonty@gmail.com>
2021-10-24 17:40:29 -0300

    agregamos maquetado en la pagina principal y la logica necesaria para mostrar los productos

---
+++ js/app.js
@@ -0,0 +1,27 @@
+let listaProductos = [];
+
+cargaInicial()
+
+function cargaInicial() {
+  //revisar los datos de local setorage
+  listaProductos = JSON.parse(localStorage.getItem("arregloProductos")) || [];
+  //dibujar las columnas con las cards
+if(listaProductos.length > 0){
+    listaProductos.forEach(itemProducto => {
+        crearColumna(itemProducto)
+    });
+}
+}
+
+function crearColumna(producto) {
+  let grilla = document.querySelector("#grilla");
+  grilla.innerHTML += ` <div class="col-sm-12 col-md-4 col-lg-3 mb-3">
+    <div class="card" >
+      <img src="${producto.url}" class="card-img-top" alt="${producto.nombre}">
+      <div class="card-body">
+        <h5 class="card-title">${producto.nombre}</h5>
+        <p class="card-text">${producto.descripcion}</p>
+      </div>
+    </div>
+  </div>`;
+}
